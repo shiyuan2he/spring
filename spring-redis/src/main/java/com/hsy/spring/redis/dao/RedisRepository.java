5625b51717bbc588f24b98ef64ebe317c9d5b9ab
shiyuan2he <shiyuan4work@sina.com>
2018-07-23 22:59:26 +0800

    spring集成redis

--- spring-redis/src/main/java/com/hsy/spring/redis/dao/RedisRepository.java
+++ spring-redis/src/main/java/com/hsy/spring/redis/dao/RedisRepository.java
@@ -2,16 +2,23 @@ package com.hsy.spring.redis.dao;
 import com.hsy.java.util.cache.redis.impl.AbstractSpringRedisCacheEnhance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository(value = "redisRepository")
 public class RedisRepository extends AbstractSpringRedisCacheEnhance {
 
-    @Autowired
-    RedisTemplate<String, Object> redisTemplate;
+    @Autowired RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired StringRedisTemplate stringRedisTemplate;
 
     @Override
     public RedisTemplate<String, Object> getRedisTemplate() {
         return redisTemplate;
+    }
+
+    @Override
+    public StringRedisTemplate getStringRedisTemplate() {
+        return stringRedisTemplate;
     }
 }
