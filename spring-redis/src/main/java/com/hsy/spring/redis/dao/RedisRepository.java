dfabcf08d6ff35e3c755335b36c84f40257905e7
shiyuan2he <shiyuan4work@sina.com>
2018-07-23 19:10:36 +0800

    redis build

--- spring-redis/src/main/java/com/hsy/spring/redis/dao/RedisRepository.java
+++ spring-redis/src/main/java/com/hsy/spring/redis/dao/RedisRepository.java
@@ -1,10 +1,11 @@
 package com.hsy.spring.redis.dao;
+import com.hsy.java.util.cache.redis.impl.AbstractSpringRedisCacheEnhance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository(value = "redisRepository")
-public class RedisRepository extends AbstractSpringRedisCacheEnhance{
+public class RedisRepository extends AbstractSpringRedisCacheEnhance {
 
     @Autowired
     RedisTemplate<String, Object> redisTemplate;
---
+++ spring-redis/src/test/java/com/hsy/spring/redis/dao/RedisRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.hsy.spring.redis.dao;
+
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path spring/com.hsy.spring.redis.dao
+ * @date 2018/7/23 18:42
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2018 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+@RunWith(JUnit4ClassRunner.class)
+@WebAppConfiguration("src/main/webapp")
+@ContextConfiguration({"classpath*:/spring/applicationContext.xml"})
+public class RedisRepositoryTest {
+
+    @Autowired private RedisRepository redisRepository;
+    @Test
+    public void testSetString(){
+        redisRepository.putCache("test:set:name","robot");
+    }
+}
